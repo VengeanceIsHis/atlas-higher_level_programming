bce15929abe4a661f0abe31c63895b3f43b555ab
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-05 11:18:42 -0800

    fix

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -10,7 +10,8 @@ while number >= 0:
         print(f"Last digit of {number} is {last_digit} and is 0")
         break
     else:
-print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+        print(f"Last digit of {number} is {last_digit} and is less than 6 and
+        not 0")
         break
 while number < 0:
     last_digit = abs(number) % 10
@@ -22,5 +23,6 @@ while number < 0:
         print(f"Last digit of {number} is {last_digit} and is 0")
         break
     else:
-print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+        print(f"Last digit of {number} is {last_digit} and is less than 6 and
+        not 0")
         break
