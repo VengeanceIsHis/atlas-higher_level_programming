02488a7157ac98f2a2669e46cfb234b130d1062f
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-05 11:43:59 -0800

    fix

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,12 +8,12 @@ elif number >= 0 and last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 elif number >= 0 and last_digit < 6 and last_digit != 0:
     print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
-elif number < 0 and last_digit > 5:
+elif number < 0 and -abs(last_digit) > 5:
     last_digit = -abs(last_digit)
     print(f"Last digit of {number} is {last_digit} and is greater than 5")
-elif number < 0 and last_digit == 0:
+elif number < 0 and -abs(last_digit) == 0:
     last_digit = -abs(last_digit)
     print(f"Last digit of {number} is {last_digit} and is 0")
 else:
     last_digit = -abs(last_digit)
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0") 
