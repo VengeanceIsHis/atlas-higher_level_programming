0e0221312b00d7cbd8bedc5e7a7bb7c3e00d72c3
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-06 12:20:48 -0800

    fix

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 import sys
-
-for arg in sys.argv:
-    count = 0
-    count += 1
-if arg == 0:
-    print("0 arguments.")
-elif arg < 2:
-    print("1 argument:")
-    print("1: {}".format(arg))
-else:
-    print("{} arguments:".format(count))
+def main():
     for arg in sys.argv:
-        i = 1
-        if arg > 0:
-            print("{}: {}".format(i, arg))
+        count = 0
+        count += 1
+    if arg == 0:
+        print("0 arguments.")
+    elif arg < 2:
+        print("1 argument:")
+        print("1: {}".format(arg))
+    else:
+        print("{} arguments:".format(count))
+        for arg in sys.argv:
+            i = 1
+            if arg > 0:
+                print("{}: {}".format(i, arg))
+if __name__ == "__main__":
+    main()
