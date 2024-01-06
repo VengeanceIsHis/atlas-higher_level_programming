02632a74e83e3a71e6d63a29fb1c954fc087972e
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-06 11:47:56 -0800

    fix

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from add_0.py import add
+from add_0 import add
 def main():
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b))
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
 if __name__ == "__main__":
-          main()
+    main()
