4d1b598e53f9e08a5924222a391451b996d8c160
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-06 12:43:34 -0800

    fix

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,11 +3,11 @@ import sys
 
 
 def main():
-    result = 0;
+    result = 0
     for i, arg in enumerate(sys.argv[1:], start=1):
         result += int(sys.argv[i])
     print(f"{result}")
 
-    
+
 if __name__ == "__main__":
     main()
