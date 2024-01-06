6f073abfb780a28b1ee2c61d3ebaa9a85a1d6c68
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-05 17:38:30 -0800

    fix

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i < 10:
-        print("0{}".format(i), end='')
+    print("{:02d}".format(i), end='')
     if i < 99:
-        print(", ", end='')
+        print(", ")
