f90a089b826c6f3ab43777c27d896fcb36828f2e
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-05 18:05:09 -0800

    fix

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for i in range(10):
-    for j in range(i, 10):
-        if i == j:
-            two_digit_number = f"{i:02d}"
+    for j in range(i + 1, 10):
+            two_digit_number = f"{i:02d}{j:02d}"
             print(two_digit_number)
