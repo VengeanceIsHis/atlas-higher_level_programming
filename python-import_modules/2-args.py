0080735a17ef7959399ee60887cc6468b01d4190
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-06 12:24:08 -0800

    fix

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 import sys
 def main():
+    count = -1
     for arg in sys.argv:
-        count = 0
         count += 1
-    if arg == 0:
+    if count == 0:
         print("0 arguments.")
-    elif arg < 2:
+    elif count < 2:
         print("1 argument:")
         print("1: {}".format(arg))
     else:
         print("{} arguments:".format(count))
+        2count = -1
         for arg in sys.argv:
+            2count += 1
             i = 1
-            if arg > 0:
+            if 2count > 0:
                 print("{}: {}".format(i, arg))
 if __name__ == "__main__":
     main()
