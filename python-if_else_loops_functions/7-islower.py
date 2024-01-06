95554d8025bd15b65829964144b4b3c8649e53bc
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-05 18:44:25 -0800

    fix

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -2,7 +2,7 @@
 def islower(c):
     if (ord(c) > 64 and ord(c) < 91):
         return False
-    elif (ord(c) > 96 and ord(c) < 123)
+    elif (ord(c) > 96 and ord(c) < 123):
         return True
-    else
+    else:
         return False
