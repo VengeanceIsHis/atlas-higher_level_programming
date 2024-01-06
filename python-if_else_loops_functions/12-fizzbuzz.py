3f63c9a21aa2a21338de346421f57ad58cb6e801
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-05 20:14:52 -0800

    fix

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 for i in range(1, 101):
     if (i % 3 == 0):
-        print("Fizz")
+        print("Fizz ", end='')
     elif (i % 5 == 0):
-        print("Buzz")
+        print("Buzz ", end='')
     elif (i % 3 == 0 and i % 5 == 0):
-        print("FizzBuzz")
+        print("FizzBuzz ", end='')
     else:
         print("{}".format(i), end=' ')
