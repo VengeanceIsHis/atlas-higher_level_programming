7d83359dd3b54e0d3ed9248772a4b0b2b88bf82b
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-06 17:59:41 -0800

    fix

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 import hidden_4 as hidden
 def main():
-    dir(hidden)
+    mo_names = dir(hidden)
+    for name in mo_names:
+        print(name)
+
+
 if __name__ == "__main__":
     main()
