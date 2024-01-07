80c5fe2b54e08259157c26614065d7770546dce9
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-07 11:51:41 -0800

    fix

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3                                                              
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a(0, ) = tuple_a(0, ) + tuple_b(0, )
+    return tuple_a
+if __name__ == "__main__":
+    main()
+
+
