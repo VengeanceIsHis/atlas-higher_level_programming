d70e6585d846d0c2c5e2a20262ff844ace1db538
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-07 10:46:42 -0800

    fix

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    my_string = my_string.replace('C', ''))
+    my_string = my_string.replace('c', ''))
+    return my_string
+    
+
+if __name__ == "__main__":
+    main()
