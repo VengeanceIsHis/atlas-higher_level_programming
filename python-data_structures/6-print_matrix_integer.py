29675e8fc8a6b74dd378a521bf5d76c116db8f81
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-07 11:20:45 -0800

    fix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for value in row:
-            print("{:d}".format(value), end=' ')
+        for index, value in enumerate(row):
+            print("{:d}".format(value), end='')
+            if index < len(row) - 1:
+                print(' ', end='')
         print()
 
 if __name__ == "__main__":
