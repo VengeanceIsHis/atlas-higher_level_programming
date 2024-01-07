8206ab304d16af57b5784ab69de495e44e748e6d
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-07 11:09:00 -0800

    fix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        print(str(row).strip('[]'))
+        print(*row), sep=', ')
 
 
 if __name__ == "__main__":
