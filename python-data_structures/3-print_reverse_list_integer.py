a03cbf041252aa8ecfef2daed91a34a2dc4a3cb5
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-06 18:56:23 -0800

    fix

---
+++ python-data_structures/3-print_reverse_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_reserved_list_integer(my_list=[]):
+    for i in reversed(range(len(my_list) + 1)):
+        print("{}".format(my_list))
+
+
+
+
+if __name__ == "__main__":
+    main()
