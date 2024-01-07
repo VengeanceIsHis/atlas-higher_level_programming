4bbed786bd945078c33a09d529a18e8ed19e8405
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-07 13:28:01 -0800

    fix

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     for i in my_list:
-        if (my_list[i] % 2) == 0):
+        if (my_list[i] % 2) == 0:
             true_list[i] = True
         else:
             true_list[i] = False
