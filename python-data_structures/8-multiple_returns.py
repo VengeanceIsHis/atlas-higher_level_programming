0acab918f327c871f6d428a3554f69a315ca2a69
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-07 13:10:19 -0800

    fix

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,10 +3,12 @@ def multiple_returns(sentence):
     if sentence:
         for i, char in enumerate(sentence):
             if i == 0:
-                firstchar = char    
+                firstchar = char
         length = i + 1
         return length, firstchar
     else:
         return 0, None
+
+
 if __name__ == "__main__":
     main()
