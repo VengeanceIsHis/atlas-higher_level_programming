f94f819388752134ef93a0fac06b3fe3a0ea074a
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-06 18:57:17 -0800

    fix

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reserved_list_integer(my_list=[]):
     for i in reversed(range(len(my_list) + 1)):
-        print("{}".format(my_list))
+        print("{:d}".format(my_list))
 
 
 
