b202d5815244aee4485dc87a3ecd498a0afafac8
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-06 18:07:08 -0800

    fix

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,10 +3,7 @@ import hidden_4 as hidden
 def main():
     mo_names = dir(hidden)
     for name in mo_names:
-        for i in name:
-            if name =='_'
-                break
-            else:
+            if not name.startswith('_'):
                 print(name)
 
 
