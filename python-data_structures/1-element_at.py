72a222070addda363f4b7cd8fa4a3567102f907a
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-06 18:46:04 -0800

    fix

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
+    if idx < 0 or idx >= len(my_list):
         return None
-    count = 0
-    for i in my_list:
-        count += 1
-    if idx > count:
-        return None
-    else:
-        for i in my_list[:idx]:
-            print("{}".format(i))
+    return my_list[idx]
