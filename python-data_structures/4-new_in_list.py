07384ccc93d213923920aee991016f34d3762198
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-07 10:29:21 -0800

    fix

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -5,7 +5,7 @@ def new_in_list(my_list, idx, element):
         return new_list
     else:
         new_list[idx] = element
-        return new_list
+        return new_list, my_list
 
 if __name__ == "__main__":
     main()
