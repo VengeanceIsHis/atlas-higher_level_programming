8ceeaa66d2e73ec5483986584a677f4936785220
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-07 12:22:35 -0800

    fix

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3                                                              
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) > 2:
-       len1 = 2
-    elif len(tuple_b) > 2:
+    len1 = len(tuple_a)
+    len2 = len(tuple_b)
+
+    if len1 > 2:
+        len1 = 2
+    if len2 > 2:
         len2 = 2
-    else:
-       len1 = len(tuple_a)
-       len2 = len(tuple_b)
-    if len2 > len1:
-        truelen = len2
-    else:
-        truelen = len1
-    i = 0
-    tuple1 = tuple_a[i] + tuple_b[i]
-    tuple2 = tuple_a[i + 1] + tuple_b[i + 1]
+    truelen = max(len1, len2)
+
+    tuple1 = tuple_a[0] + tuple_b[0]
+    tuple2 = tuple_a[1] + tuple_b[1]
     result_tuple = (tuple1, tuple2)
     return result_tuple
 if __name__ == "__main__":
