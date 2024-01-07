ebe3f2743458eff2ae9de4e7a174b12a90598cf2
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-07 13:38:48 -0800

    pycode validate

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    true_list=[False] * len(my_list)
+    true_list = [False] * len(my_list)
     for i, value in enumerate(my_list):
         if value % 2 == 0:
             true_list[i] = True
         else:
             true_list[i] = False
     return true_list
-
-
-
-
-
 
 
 if __name__ == "__main__":
