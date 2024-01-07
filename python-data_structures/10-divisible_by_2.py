72749a2148482d5bd0c0b9a5342bea06b7ad75c6
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-07 13:27:23 -0800

    fix

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    for i in my_list:
+        if (my_list[i] % 2) == 0):
+            true_list[i] = True
+        else:
+            true_list[i] = False
+    return true_list
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
