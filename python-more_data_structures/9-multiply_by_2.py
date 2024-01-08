517d7982290970cdbfb5f74d47d3c4124a832947
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-08 12:54:42 -0800

    fix

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -2,6 +2,6 @@
 def multiply_by_2(a_dictionary):
     new_dir = a_dictionary.copy()
     list_keys = list(new_dir.keys())
-    for key in list_keys:
-        new_dir[key] *= 2
-    return (a_dictionary)
+    for i in list_keys:
+        new_dir[i] *= 2
+    return (new_dir)
