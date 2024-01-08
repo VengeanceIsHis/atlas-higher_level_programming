227eed88f4c5e65491be22b7c830cc3701fb6eb1
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-08 10:51:57 -0800

    fix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,5 +3,5 @@ def square_matrix_simple(matrix=[]):
     squared_matrix = matrix.copy()
 
     for i in range(len(matrix)):
-        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+        squared_matrix[i] = list(map(lambda x: x**2, matrix[i]))
     return squared_matrix
