55f50b5ce10722cbd7672f84f8d9c7317d8c3281
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-08 11:48:24 -0800

    fix

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    my_list = []
-    length = max(len(set_1),len(set_2))
-    for i in length:
-        if set_1[i] == set_2[i]:
-            my_list.append(set_1[i])
-    return my_list
+    return set_1 & set_2
