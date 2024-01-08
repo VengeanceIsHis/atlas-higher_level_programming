a3d1c4d4b8d7af47709e2ca50bf6ba620029c888
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-08 15:10:03 -0800

    fix

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    result = 0
+    for i in roman_string:
+        if roman_string[i] = "I":
+            result += 1
+        elif roman_string[i] = "V":
+            result += 5
+        elif roman_string[i] = "X":
+            result += 10
+        elif roman_string[i] = "L":
+            result += 50
+        elif roman_string[i] = "C":
+            result += 100
+        elif roman_string[i] = "D":
+            result += 500
+        elif roman_string[i] = "M":
+            result += 1000
+        else:
+            continue
+    return result
