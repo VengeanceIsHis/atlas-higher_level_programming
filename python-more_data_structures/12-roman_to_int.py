10ebe0c73816c1c70504c051e102cf3189e3dfcb
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-08 15:34:46 -0800

    fix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,30 +1,40 @@
 #!/usr/bin/python3
+def to_subtract(list_num):
+    to_sub = 0
+    max_list = max(list_num)
+
+    for n in list_num:
+        if max_list > n:
+            to_sub += n
+
+    return (max_list - to_sub)
+
+
 def roman_to_int(roman_string):
-    result = 0
-    prev_value = 0
-    current_value = 0
     if not roman_string:
-        return None
-    for char in roman_string:
-        if char == 'I':
-            current_value += 1
-        elif char == 'V':
-            current_value += 5
-        elif char == 'X':
-            current_value += 10
-        elif char == 'L':
-            current_value += 50
-        elif char == 'C':
-            current_value += 100
-        elif char == 'D':
-            current_value += 500
-        elif char == 'M':
-            current_value += 1000
-        else:
-            continue
-        if current_value > prev_value:
-            result += current_value - 2 * prev_value
-        else:
-            result += current_value
-            prev_value = current_value
-    return result
+        return 0
+
+    if not isinstance(roman_string, str):
+        return 0
+
+    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    list_keys = list(rom_n.keys())
+
+    num = 0
+    last_rom = 0
+    list_num = [0]
+
+    for ch in roman_string:
+        for r_num in list_keys:
+            if r_num == ch:
+                if rom_n.get(ch) <= last_rom:
+                    num += to_subtract(list_num)
+                    list_num = [rom_n.get(ch)]
+                else:
+                    list_num.append(rom_n.get(ch))
+
+                last_rom = rom_n.get(ch)
+
+    num += to_subtract(list_num)
+
+    return (num)
