4e7a16d5fd9c1fc6e5102646014ed205443729b9
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-08 15:12:02 -0800

    fix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,19 +2,19 @@
 def roman_to_int(roman_string):
     result = 0
     for i in roman_string:
-        if roman_string[i] = "I":
+        if roman_string[i] = 'I':
             result += 1
-        elif roman_string[i] = "V":
+        elif roman_string[i] = 'V':
             result += 5
-        elif roman_string[i] = "X":
+        elif roman_string[i] = 'X':
             result += 10
-        elif roman_string[i] = "L":
+        elif roman_string[i] = 'L':
             result += 50
-        elif roman_string[i] = "C":
+        elif roman_string[i] = 'C':
             result += 100
-        elif roman_string[i] = "D":
+        elif roman_string[i] = 'D':
             result += 500
-        elif roman_string[i] = "M":
+        elif roman_string[i] = 'M':
             result += 1000
         else:
             continue
