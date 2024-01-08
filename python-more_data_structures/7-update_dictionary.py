0fb944ab9e8747f2174120d7ec4f6139332facbe
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-08 12:27:03 -0800

    fix

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    keys_list = list(a_dictionary.keys())
-    result = 0
-    for i in keys_list:
-        if keys_list[i] == key:
-            result = 1
-            keys_list.remove(keys_list[i])
-        if result == 1:
-            keys_list.append(key)
-            my_dict[key] = value
-        new_dict = dict(zip(keys_list, a_dictionary.values()))
-        return new_dict
+    a_dictionary[key] = value
+    return (a_dictionary)
