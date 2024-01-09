9ece2efaa1b13d9888fa94d760647e228aeb4c42
VengeanceIsHis <elijahjpoole@gmail.com>
2024-01-08 17:08:12 -0800

    fix

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x = 0):
     try:
         for index in my_list:
-        print("{}".format(index))
+        print("{}".format(index), end='')
         if index == x:
             break:
     return index
