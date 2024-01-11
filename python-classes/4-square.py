54139ebde8c1eba6c3401bedd9de13521a598471
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-11 13:15:36 -0600

    Indention fix

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -19,6 +19,6 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-        def area(self):
-           """Gathers the area of square"""
+    def area(self):
+        """Gathers the area of square"""
         return (self.__size * self.__size)
