9efbb6a6a36bbd5f0c0f9ad165a79863810eb24d
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-11 13:04:44 -0600

    Test of the get attr

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -10,6 +10,14 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-        def area(self):
-            return size * size
+
+    def area(self):
+        return (self.__size * self.__size)
+    def size(self):
+        return getattr(self.__size)
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
         
