43a74a221ec6c13868876e0ed082ffac3540ad38
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-11 12:17:42 -0600

    1st Square with data

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module of Square"""
+
+
+class Square:
+    """Class as a whole"""
+    def __init__(self, size):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
