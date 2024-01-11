8b8b13758616b5eba7de796ecdb7b639f30dc594
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-11 13:30:21 -0600

    2nd test

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -26,6 +26,8 @@ class Square:
         """Gathers the area of square"""
         return (self.__size * self.__size)
     def my_print(self):
-        for row in size:
-            for i in row:
-                print("#")
+        for i in range (0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
