e49b7a36c5c905f840d024f7da7b58d978b867e1
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-11 14:48:30 -0600

    pycode validation

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -22,7 +22,7 @@ class Square:
             raise ValueError("size must be >= 0")
 
         self.__size = value
+
     def area(self):
         """Gathers the area of square"""
         return (self.__size * self.__size)
-    
