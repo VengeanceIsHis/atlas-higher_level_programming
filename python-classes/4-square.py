33b107e49222ff1c3bd303dac486026b88c16426
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-11 13:23:45 -0600

    Pycode fix

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -7,10 +7,12 @@ class Square:
     def __init__(self, size=0):
         """New Square initialized"""
         self.__size = size
+
     @property
     def size(self):
         """gets current size of the square"""
         return self.__size
+
     @size.setter
     def size(self, value):
         """Redefines the size of a square"""
@@ -18,7 +20,9 @@ class Square:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
+
         self.__size = value
     def area(self):
         """Gathers the area of square"""
         return (self.__size * self.__size)
+    
