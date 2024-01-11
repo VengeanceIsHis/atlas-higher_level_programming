0bc5c25e3d60bc54559adfefc7e9015687714f86
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-11 13:45:57 -0600

    fix

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -22,13 +22,14 @@ class Square:
             raise ValueError("size must be >= 0")
 
         self.__size = value
-  
+
     def area(self):
         """Gathers the area of square"""
-        
+
         return self.__size * self.__size
+
     def my_print(self):
-        for i in range (0, self.__size):
+        for i in range(0, self.__size):
             [print("#", end="") for j in range(self.__size)]
             print("")
         if self.__size == 0:
