3278e1dec4823eb99c016c814752718ffd876ba3
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-11 12:38:32 -0600

    Test

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,4 +7,4 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.size = size
+        self.__size = size
