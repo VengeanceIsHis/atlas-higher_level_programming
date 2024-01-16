786636753464dde42fe9aae0b3b1fa750484646d
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-15 23:33:52 -0600

    Fix

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -10,7 +10,10 @@ def print_square(size):
         raise ValueError("size must be >= 0")
     if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
-    for row in size:
-        for j in row:
-            print("#", end="")
+    if size == 0:
         print("")
+    else:
+        for row in size:
+            for i in row:
+                print("#", end="")
+            print("")
