0f8b122519deaa438328e885f31a8c1e917016c1
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-21 16:00:45 -0600

    New file

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,6 +3,10 @@
 
 class MyList (list):
     """New class for printing sorted list"""
+    
+    
     def print_sorted(self):
-    """Sorted print of the current instance"""
-    print(sorted(self))
+        """Sorted print of the current instance"""
+        
+        
+        print(sorted(self))
