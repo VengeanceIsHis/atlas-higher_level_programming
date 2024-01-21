29a52732871d8c4b0524e73e4fa0beb928a6d606
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-21 15:54:08 -0600

    Super hero

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Module that creats a subclass to list"""
 
-
-def print_sorted(self):
+class MyList (list):
+    """New class for printing sorted list"""
+    def print_sorted(self):
     """Sorted print of the current instance"""
     print(sorted(self))
