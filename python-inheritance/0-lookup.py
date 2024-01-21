2dee361a95b24d5be6729caed6caf388eb15b275
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-20 23:15:45 -0600

    Watch me cook

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 """Module for new function"""
+
+
 def lookup(obj):
-    """New functino that returns all atrributes"""
-    return (dir(obj))
+    """New functino that rereturn (dir(obj))turns all atrributes"""
+    all_attributes = lookup(obj)
+    filtered_attributes = [attr for attr in all_attributes if not (attr.startswith("__") and attr.endswith("__"))]
+    return filtered_attributes
