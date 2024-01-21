0ea3458afc73b3e16ec7ea9ae1001664a36d9fb3
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-21 15:45:59 -0600

    Call in

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that creats a subclass to list"""
+
+
+Class MyList(list):
+    def __init__(self)
+    my_list = super().__init__()
+    def print_sorted(self):
+        print(my_list)
