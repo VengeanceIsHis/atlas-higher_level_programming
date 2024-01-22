113f3abb3790970ebd12f2780fc02293a00b5ab8
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-22 11:07:20 -0600

    New push for answer 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""New module for a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """New class made"""
+    def area(self):
+        raise Exception("area() is not implemented")
