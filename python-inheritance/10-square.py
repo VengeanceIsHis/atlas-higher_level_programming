43172b1ab7dd2e1cd77279a44149343797040940
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-22 14:04:56 -0600

    Fixed

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """New module for sublass Rectangle from Geometry."""
-Rectangle = __import__('9-rectangle.py').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
