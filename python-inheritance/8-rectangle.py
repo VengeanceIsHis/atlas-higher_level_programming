fade4e95ec168cae5813595041537444cce8d044
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-22 11:23:11 -0600

    New push for validator

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """New module for sublass Rectangle from Geometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """Initialized subclass of Rectangle"""
+    
     def __init__(self, width, height):
         """initialization process"""
-        if integer_validator(width) == True:
-            self.__width = width
-        if integer_validator(height) == True:
-            self.__height = height
+        self.integer_validator("width". width)
+        self.__width = width
+        self.integer_validator("height". height)
+        self.__height = height
