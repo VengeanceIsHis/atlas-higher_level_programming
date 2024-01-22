1e9b3ecff7c56a368b71163566a35631159a1b29
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-22 10:17:47 -0600

    MOO

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,6 +4,8 @@
 
 def is_same_class(obj, a_class):
     """Learning how to do test functions"""
+    if obj == "True" or obj == "False":
+        return False
     if not isinstance(obj, a_class):
         return False
     return True
