ad0023dd0bea9ddba5a5a6cafb34b36e5b3ef9bf
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-22 11:25:02 -0600

    Error

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -8,7 +8,7 @@ class Rectangle(BaseGeometry):
     
     def __init__(self, width, height):
         """initialization process"""
-        self.integer_validator("width". width)
+        self.integer_validator("width", width)
         self.__width = width
-        self.integer_validator("height". height)
+        self.integer_validator("height", height)
         self.__height = height
