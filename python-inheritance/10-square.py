53da83ec42d62e50f228ba6c449ec4adb26b5361
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-22 14:02:44 -0600

    Documentation

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -6,8 +6,10 @@ Rectangle = __import__('9-rectangle.py').Rectangle
 class Square(Rectangle):
     """New subclass Square from Rectangle class"""
     def __init__(self, size):
+        """initializing a Square"""
         self.integer_validator("size", int)
         if size > 0:
             self.__size = size
     def area(self):
+        """Return the area of the square"""
         return self.__size * self.__size
