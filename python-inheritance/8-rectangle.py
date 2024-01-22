b0af6a6a63eb565e66c78af2a00ae8e445eb0e04
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-22 11:18:38 -0600

    Document fixes

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """New module for sublass Rectangle from Geometry"""
+
+
 class Rectangle(BaseGeometry):
     """Initialized subclass of Rectangle"""
     def __init__(self, width, height):
+        """initialization process"""
         if integer_validator(width) == True:
             self.__width = width
         if integer_validator(height) == True:
