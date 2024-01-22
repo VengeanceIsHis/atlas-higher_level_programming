61addf7bc6540eebb05ea40143eb625643bf435b
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-22 11:00:03 -0600

    OOPS

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,6 +4,6 @@
 
 def inherits_from(obj, a_class):
     """If is a subclass return True"""
-    if issubclass(type(obj)) and type(obj) != a_class:
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
