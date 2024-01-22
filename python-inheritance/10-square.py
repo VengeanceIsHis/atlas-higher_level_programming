da344c43dcbb17950d809754e399d6e85eea0bbb
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-22 14:00:26 -0600

    10-Answer

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""New module for sublass Rectangle from Geometry"""
+Rectangle = __import__('9-rectangle.py').Rectangle
+
+
+class Square(Rectangle):
+    """New subclass Square from Rectangle class"""
+    def __init__(self, size):
+        self.integer_validator("size", int)
+        if size > 0:
+            self.__size = size
+    def area(self):
+        return self.__size * self.__size
