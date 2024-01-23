476727a80712b150a001e964debed236416eade8
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-23 10:34:15 -0600

    OOPS

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,5 +2,5 @@
 """Opening and reading a file"""
 def read_file(filename=""):
     """Function that finds the file and reads it"""
-    with open('filename', encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
