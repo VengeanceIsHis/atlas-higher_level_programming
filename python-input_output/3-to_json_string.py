8cc121998697ba836808a8d8e45dc5d882b59d3e
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-23 11:39:21 -0600

    New fix

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-import json
 """Function that changes an obj to an json string"""
+
+
+import json
 def to_json_string(my_obj):
     """This function returns the obj as the json string"""
     return json_dump(my_obj)
