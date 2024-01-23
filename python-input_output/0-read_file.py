cb5f0b12278573e98f2c6ff64a5e4a05b4deac1a
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-23 10:34:58 -0600

    PEP fix

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Opening and reading a file"""
+
+
 def read_file(filename=""):
     """Function that finds the file and reads it"""
     with open(filename, encoding="utf-8") as f:
