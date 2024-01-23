3a1b0a62ff245b4f12e9a7093fc9948800b968b5
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-23 11:26:47 -0600

    Nicely worked

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""New module for adding a string to the end of a file"""
+
+
+def append_write(filename="", text=""):
+    """Function that appends to a file or creates a new file with text added"""
+    with open(filename, 'a', encoding="utf-8") as f:
+        f.write(text)
