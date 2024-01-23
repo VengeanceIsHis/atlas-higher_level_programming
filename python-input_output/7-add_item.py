6d084ce2187244b9772299a7a4c7df8895943a64
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-23 12:50:12 -0600

    Exciting

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,7 +7,11 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-file_data = load_from_json_file('add_item.json')
+
+try:
+    file_data = load_from_json_file('add_item.json')
+except FileNotFoundError:
+    file_data =[]
 for arg in sys.argv[1:]:
     file_data.append(arg)
 save_to_json_file(file_data, 'add_item.json')
