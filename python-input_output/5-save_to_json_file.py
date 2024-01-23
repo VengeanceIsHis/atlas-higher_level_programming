ad77f423340826caaccf6c4019414d1ac5f9a4bf
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-23 12:09:41 -0600

    Interesting looks

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module utilizes json to manipulate a string into a file"""
+
+
+def save_to_json_file(my_obj, filename):
+    """This function sends an obj to a file in json format"""
+    with open(filename, 'w', encoding="utf-8") as f:
+        return json.dump(my_obj, filename)
+    
