6149d316502bc407b707b21f241fc6028d823b62
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-23 10:45:27 -0600

    Fix

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """Function that adds a string called text into file name"""
-    with open(filename, w, encoding="utf-8") as f:
+    with open(filename, 'w', encoding="utf-8") as f:
         f.write(text)
