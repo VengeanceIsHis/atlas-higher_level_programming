e81445c5082b45b8c58f49a56ed53e97f534234b
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-23 10:28:43 -0600

    Interesting

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,4 +4,5 @@ def read_file(filename=""):
     """Function that finds the file and reads it"""
     with open('filename', encoding="utf-8") as f:
         read_data = f.read()
-        return read_data
+    return read_data
+    f.closed
