5c00d47204d2047062d14c43afbd8d0f6c5e5649
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-23 12:41:30 -0600

    Python module fix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """Module that adds all arguments to a python list into a file"""
+
+
 import json
 import sys
-save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 for arg in sys.argv:
     json_list = save_to_json_file(arg, add_item.json)
 load_from_json_file(add_item.json)
