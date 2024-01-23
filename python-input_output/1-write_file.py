02c34ad7a094021d8217c193441c4206120fa773
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-23 10:39:38 -0600

    Writing code to a file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module for writing a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """Function that adds a string called text into file name"""
+    with open(filename, encoding="utf-8") as f:
+        f.write(text)
