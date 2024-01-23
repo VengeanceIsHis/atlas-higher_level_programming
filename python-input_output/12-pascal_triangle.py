bf1d90f124940ae7f2c342f044165d4374fa40f5
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-23 14:02:10 -0600

    12-pascal

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""New module for new array of Pascal Triangle"""
+def pascal_triangle(n):
+    """function that returns the pascal Triangle to be printed"""
+    if n <= 0:
+        return []
+
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
