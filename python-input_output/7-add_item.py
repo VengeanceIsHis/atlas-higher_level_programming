987ac9797e670269594bc0180c795eec0beb0f5c
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-23 12:51:48 -0600

    Awesome task

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -11,7 +11,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 try:
     file_data = load_from_json_file('add_item.json')
 except FileNotFoundError:
-    file_data =[]
+    file_data = []
 for arg in sys.argv[1:]:
     file_data.append(arg)
 save_to_json_file(file_data, 'add_item.json')
