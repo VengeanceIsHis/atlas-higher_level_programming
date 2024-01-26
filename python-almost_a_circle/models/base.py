16fdf09a96666caa98120e16ea07f672429dfbd9
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-26 13:55:41 -0600

    Retry

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -7,7 +7,7 @@ class Base:
     __nb_objects = 0
     def __init__(self, id=None):
         """initializing new instance with id"""
-        if not id == 'None':
+        if not id == None:
             self.id = id
         else:
             __nb_objects += 1
