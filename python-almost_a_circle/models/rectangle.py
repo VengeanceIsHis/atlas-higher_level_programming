109a5622d0c6fe5c29696ae0aad9e04242f7157f
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-26 17:01:54 -0600

    New fix

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -7,6 +7,7 @@ class Rectangle(Base):
 
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializing a new rectangle"""
         self.__width = width
         self.__height = height
         self.__x = x
