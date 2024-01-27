b5211abab490e0b2ad07cf8a23d1478261fd4d73
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-26 19:04:17 -0600

    Loop fix

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -72,7 +72,7 @@ class Rectangle(Base):
 
     def display(self):
         """New display function that prints out the shape"""
-        for row in self.width:
-            for col in self.height:
+        for row in range(0, self.width):
+            for col in range(0, self.height):
                 print('#', end="")
             print()
