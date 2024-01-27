9432745e65e5511909e5aab13888035d47c38ebc
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-26 18:12:24 -0600

    Get er done

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""New module for class Base"""
+
+
+class Base:
+   """New class that creats the foundation for the Project"""
+   __nb_objects = 0
+
+
+   def __init__(self, id=None):
+       """initializing new instance with id"""
+       if id is not None:
+           self.id = id
+       else:
+           Base.__nb_objects += 1
+           self.id = Base.__nb_objects
---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""Module for rectangle as a subclass to Base"""
+
+
+class Rectangle(Base):
+   """initializing new instance of Rectangle"""
+
+
+   def __init__(self, width, height, x=0, y=0, id=None):
+       """Initializing a new rectangle"""
+       self.width = width
+       self.height = height
+       self.x = x
+       self.y = y
+       super().__init__(id)
+
+
+   @property
+   def width(self):
+       """returns the width"""
+       return self.__width
+  
+   @width.setter
+   def width(self, value):
+       """sets the width"""
+       if not isinstance(value, int):
+           raise TypeError("size must be an integer")
+       elif value < 0:
+           raise ValueError("size must be >= 0")
+       self.__width = value
+  
+   @property
+   def height(self, value):
+       """returns the height"""
+       return(self.__height)
+
+
+   @height.setter
+   def height(self, value):
+       """sets the height"""
+       if not isinstance(value, int):
+           raise TypeError("size must be an integer")
+       elif value < 0:
+           raise ValueError("size must be >= 0")
+       self.__height = value
+
+
+   @property
+   def x(self):
+       """returns location x"""
+       return self.__x
+  
+   @x.setter
+   def x(self, value):
+       """sets the location x"""
+       if not isinstance(value, int):
+           raise TypeError("size must be an integer")
+       elif value < 0:
+           raise ValueError("size must be >= 0")
+       self.__height = value
+
+
+   @property
+   def y(self):
+       """returns the location variable y"""
+       return self.__y
+
+
+   @y.setter
+   def y(self, value):
+       """Sets the location y"""
+       if not isinstance(value, int):
+           raise TypeError("size must be an integer")
+       elif value < 0:
+           raise ValueError("size must be >= 0")
+       self.__y = value
