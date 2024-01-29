d2dc95af078dedafb114912a049422f066dbc892
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-29 12:29:58 -0600

    Test

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -25,36 +25,32 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
-    def update(self, *args, *kwargs):
+    def update(self, *args, **kwargs):
         """Function that updates all attributes in an instance called to Square"""
         if args and len(args) != 0:
             a = 0
             for arg in args:
                 if a == 0:
                     if arg is None:
-                        self.__init__(self.width, self.height, self.x, self.y)
+                        self.__init__(self.size, self.x, self.y)
                     else:
                         self.id = arg
                 elif a == 1:
-                    self.width = arg
+                    self.size = arg
                 elif a == 2:
-                    self.height = arg
-                elif a == 3:
                     self.x = arg
-                elif a == 4:
+                elif a == 3:
                     self.y = arg
                 a += 1
         elif kwargs and len(kwargs) != 0:
             for k, v in kwargs.items():
                 if k == "id":
                     if v is None:
-                        self.__init__(self.width, self.height, self.x, self.y)
+                        self.__init__(self.size, self.x, self.y)
                     else:
                         self.id = v
-                elif k == "width":
-                    self.width = v
-                elif k == "height":
-                    self.height = v
+                elif k == "size":
+                    self.size = v
                 elif k == "x":
                     self.x = v
                 elif k == "y":
