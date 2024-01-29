4446c347912628d21d1e84591cde8543c602a069
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-29 13:24:06 -0600

    Push

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """New module for class Base"""
 
-
 class Base:
-   """New class that creats the foundation for the Project"""
-   __nb_objects = 0
+    """New class that creates the foundation for the Project"""
+    __nb_objects = 0
 
+    def __init__(self, id=None):
+        """Initializing a new instance with id"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
-   def __init__(self, id=None):
-       """initializing new instance with id"""
-       if id is not None:
-           self.id = id
-       else:
-           Base.__nb_objects += 1
-           self.id = Base.__nb_objects
+    def to_json_string(list_dictionaries):
+        """New function that returns the json representatino of list_dictionaries"""
+        return json.dumps(list_dictionaries)
