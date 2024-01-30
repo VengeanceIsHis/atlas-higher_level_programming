a3163550c52647eefda6cb03d5ef88d9eab1f367
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-01-29 19:57:36 -0600

    New changes

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -20,7 +20,7 @@ class Base:
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
-
+    @classmethod
     def save_to_file(cls, list_objs):
         """New function that saves a list of objects into a set file"""
         filename = cls.__name__ + ".json"
