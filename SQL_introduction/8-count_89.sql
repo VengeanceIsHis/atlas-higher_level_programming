8e4f39e8ce351e67fbb6c7d4d0418ff7c28b5bb7
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-02-15 12:15:41 -0600

    NEW TEST

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- Introduce number of recordds that follow a certain guideline.
-SELECT COUNT(*) AS record_count FROM first_table;
+SELECT COUNT(*) AS record_count FROM first_table WHERE id = `89`;
