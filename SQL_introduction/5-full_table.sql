98a2c97de9f96ec666598eae3e5ed30ded9ad2bc
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-02-15 11:45:25 -0600

    Fix

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,5 @@
 -- Onliny showing the description of a table.
 USE hbtn_0c_0;
-SHOW COLUMNS FROM first_table;
+SELECT COLOUMN_NAME, DATAT_TYPE, CHARACTER_MAXIMUM_LENGTH
+FROM first_table.COLUMNS
+WHERE TABLE_NAME = first_table;
