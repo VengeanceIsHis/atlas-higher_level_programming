213fc5b587e6ac2876c3ff53345b2b4a8bdb9c18
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-02-16 13:09:38 -0600

    Inserted rows

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,13 @@
 -- Creates a second table in database.
-USE hbtn_test_db_9;
-CREATE TABLE IF NOT EXISTS second_table (
+
+CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
     id INT,
     name VARCHAR(256),
     score INT,
-)
+);
+
+INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES
+(1, 'John', 10),
+(2, 'Alox', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
