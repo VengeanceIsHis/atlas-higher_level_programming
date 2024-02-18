f98c055afc21ce240c05d933721dc035490a884f
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-02-18 11:38:12 -0600

    OOPS

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- CREATING NEW USER WITH PRIVILEGES AND PASSWORD.
-CRAETE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON * TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
