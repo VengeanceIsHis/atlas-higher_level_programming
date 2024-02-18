4b23350b64bb1d4c030dab5ee37c33c885ef3187
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-02-18 11:42:51 -0600

    BOOTH

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
 -- CREATING NEW USER WITH PRIVILEGES AND PASSWORD.
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
