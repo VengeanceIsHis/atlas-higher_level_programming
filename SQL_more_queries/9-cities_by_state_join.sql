268c6e98093f5c860ab9839cb63eb01075f2c3c3
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-02-22 12:59:57 -0600

    RIGHt

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
 -- Lists all cities in my database hbtn_0d_usa
-SELECT cities.id, cities.name, states.name
-FROM cities, states
+SELECT cities.id, cities.name AS city_name, states.name AS state_name
+FROM cities
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
