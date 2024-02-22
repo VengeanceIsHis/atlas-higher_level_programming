dbcb3f9c2c6169d49ccfe6e29421910b088eebd6
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-02-22 12:33:25 -0600

    MAYBE

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,5 @@
 -- CREATE A NEW TABLE REFERENCING CALI
 SELECT citites.city_name
 FROM cities
-JOIN states ON cities.state_id = states.id
 WHERE states.state_name = 'California'
 ORDER BY cities.id ASC;
