d4875513b93144cb649964feb4992949388da5c4
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-02-22 13:08:51 -0600

    BANG

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- SCRIPTS THAT LISTS SHOW  IN A SET DATABASE
 SELECT tv_shows.title, tb_show_genres.genre_id
-FROM hbtn_0d_tvshows
+FROM tv_shows
 ORDER BY tv_shows.title, tv_shows_genres.genre_id ASC;
