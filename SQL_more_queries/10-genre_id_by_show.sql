f0d55f14d083f1ca1c784c7c71cfcf210a0b72b5
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-02-22 13:13:59 -0600

    DISGUSTING

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,5 @@
 -- SCRIPTS THAT LISTS SHOW  IN A SET DATABASE
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
+INNER JOIN tv_show_genres ON tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_shows_genres.genre_id ASC;
