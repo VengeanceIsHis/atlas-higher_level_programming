721d32ae28c4cb68b54decdf7f4fc1959bec72b7
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-21 21:34:21 -0500

    testing for task 1

--- javascript-warm_up/1-multi_language.js
+++ javascript-warm_up/1-multi_language.js
@@ -1,4 +1,7 @@
 #!/usr/bin/nodejs
-const c = "C is fun"
-const Python = "Python is cool"
-const Java = "Javascript is amazing"
+const c = "C is fun";
+const Python = "Python is cool";
+const Java = "Javascript is amazing";
+console.log(c);
+console.log(Python);
+console.log(Java);
