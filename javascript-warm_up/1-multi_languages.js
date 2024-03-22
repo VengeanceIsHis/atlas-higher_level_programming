214f13d128497ce3c8a83f6136caefcfe718c094
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-21 21:40:47 -0500

    fixed name for java

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/nodejs
 const c = "C is fun";
 const Python = "Python is cool";
-const Java = "Javascript is amazing";
+const Java = "JavaScript is amazing";
 console.log(c);
 console.log(Python);
 console.log(Java);
