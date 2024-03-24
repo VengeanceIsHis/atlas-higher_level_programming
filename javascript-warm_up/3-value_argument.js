5d5c3ab7e724f4827cc89f317f1ebbea0bdffefb
VengeanceIsHis <elijahjpoole@gmail.com>
2024-03-24 14:18:26 -0500

    semi standard

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,6 +1,3 @@
 #!/usr/bin/nodejs
 const argument = process.argv;
-if (argument[2])
-    console.log(argument[2]);
-else
-    console.log("No argument");
+if (argument[2]) { console.log(argument[2]); } else { console.log('No argument'); }
