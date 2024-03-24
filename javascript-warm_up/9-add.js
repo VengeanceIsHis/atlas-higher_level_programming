8b6879f7670d9dccdbf5bef018af2e657c2289b5
VengeanceIsHis <elijahjpoole@gmail.com>
2024-03-24 18:48:25 -0500

    semi standard

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -3,11 +3,11 @@ const args = process.argv;
 const param1 = parseInt(args[2]);
 const param2 = parseInt(args[3]);
 let int_return = 0;
-function add(a, b) {
+function add (a, b) {
   let sum = 0;
   sum += a;
   sum += b;
   return (sum);
 }
 int_return = add(param1, param2);
-console.log(int_return)
+console.log(int_return);
