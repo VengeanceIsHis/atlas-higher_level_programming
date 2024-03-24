47bf4a2d9e317972661beb065ecb6c828dba5e82
VengeanceIsHis <elijahjpoole@gmail.com>
2024-03-24 18:16:19 -0500

    fix

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,20 +1,17 @@
 #!/usr/bin/nodejs
 const args = process.argv;
-let param = args[2];
-let i = 0, j = 0;
-let string = "";
-if (parseInt(param))
-{
-    while (i < param)
-    {
-        let temp_string = "";
-        while (j < param)
-        { 
-            temp_string += 'X';
-            j++;
-        }
-        string = temp_string;
-        console.log(string);
-        i++;
+const param = args[2];
+let i = 0; let j = 0;
+let string = '';
+if (parseInt(param)) {
+  while (i < param) {
+    let temp_string = '';
+    while (j < param) {
+      temp_string += 'X';
+      j++;
     }
+    string = temp_string;
+    console.log(string);
+    i++;
+  }
 }
