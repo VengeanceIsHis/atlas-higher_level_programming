eeba1d3aafbe755b30c2471baa8b3823df4f4f57
VengeanceIsHis <elijahjpoole@gmail.com>
2024-03-24 14:55:03 -0500

    semistandard

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/nodejs
 let i = 0;
-const arrayofcode = ["C is fun", "Python is cool", "JavaScript is amazing"];
-while (i < arrayofcode.length)
-    console.log(arrayofcode[i]);
-    i++;
+const arrayofcode = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+while (i < arrayofcode.length) { console.log(arrayofcode[i]); }
+i++;
