ce4e46e4b39706edef4296093fc0a0e63036d73b
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-24 13:43:46 -0500

    test for 2

--- javascript-warm_up/2.arguments.js
+++ javascript-warm_up/2.arguments.js
@@ -1,2 +1,8 @@
 #!/usr/bin/nodejs
-let result
+const numofArgs = process.argv.length - 2;
+if (numofArgs == 0)
+    console.log("No argument")
+if (numofArgs == 0)
+    console.log("Argument found")
+else
+    console.log("Arguments found")
