39f0c601de131bd51037c62f6aacccd6a83aacc2
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-24 14:49:24 -0500

    console.log test for 6

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/nodejs
+let i = 0;
+const arrayofcode = ["C is fun", "Python is cool", "JavaScript is amazing"];
+while (i < arrayofcode.length)
+    console.log(arrayofcode[i]);
+    i++;
