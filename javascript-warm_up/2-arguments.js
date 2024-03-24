0b9e5f1b35c3d525434e685628417b26b145d5a7
VengeanceIsHis <elijahjpoole@gmail.com>
2024-03-24 13:54:58 -0500

    fix

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,8 +1,3 @@
 #!/usr/bin/nodejs
 const numofArgs = process.argv.length - 2;
-if (numofArgs === 0)
-    console.log("No argument")
-else if (numofArgs === 1)
-    console.log("Argument found")
-else
-    console.log("Arguments found")
+if (numofArgs === 0) { console.log('No argument'); } else if (numofArgs === 1) { console.log('Argument found'); } else { console.log('Arguments found'); }
