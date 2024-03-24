19bdc2d90394762b228dd1d587a3bd1be97b182c
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-24 13:46:39 -0500

    fixed if statement

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 const numofArgs = process.argv.length - 2;
 if (numofArgs == 0)
     console.log("No argument")
-if (numofArgs == 0)
+if (numofArgs == 1)
     console.log("Argument found")
 else
     console.log("Arguments found")
