d0bbaf146ac6bbe8bd02c7ef7a42f0cc8b488c61
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-24 13:48:32 -0500

    three = signs

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/nodejs
 const numofArgs = process.argv.length - 2;
-if (numofArgs == 0)
+if (numofArgs === 0)
     console.log("No argument")
-else if (numofArgs == 1)
+else if (numofArgs === 1)
     console.log("Argument found")
 else
     console.log("Arguments found")
