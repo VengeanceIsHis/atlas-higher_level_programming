c3da8165e85ebb835ebad2f207661de242152705
VengeanceIsHis <elijahjpoole@gmail.com>
2024-03-24 14:38:39 -0500

    fix to semistandard

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,7 +1,4 @@
 #!/usr/bin/nodejs
-let args = process.argv;
-let param = args[2]
-if (parseInt(param))
-    console.log("My number: " + param)
-else
-    console.log("Not a number")
+const args = process.argv;
+const param = args[2];
+if (parseInt(param)) { console.log('My number: ' + param); } else { console.log('Not a number'); }
