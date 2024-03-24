e189f1c595891545059666c89e368f8ef57a2a5d
VengeanceIsHis <elijahjpoole@gmail.com>
2024-03-24 18:38:40 -0500

    standard error:

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,11 +1,10 @@
 #!/usr/bin/nodejs
 const args = process.argv;
-let param1 = args[2];
-let param2 = args[3];
+const param1 = args[2];
+const param2 = args[3];
 let sum = 0;
-function add(a, b)
-{
-    sum += a;
-    sum += b;
-    console.log(sum);
+function add (a, b) {
+  sum += a;
+  sum += b;
+  console.log(sum);
 }
