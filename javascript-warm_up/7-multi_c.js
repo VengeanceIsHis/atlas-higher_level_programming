59bc8e47f109ea8ad524c5984b223b3088f9d527
VengeanceIsHis <elijahjpoole@gmail.com>
2024-03-24 17:47:35 -0500

    fix merge

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,11 +2,9 @@
 const args = process.argv;
 const param = args[2];
 let i = 0;
-if (parseInt(param))
-{
-    while (i < x)
-    {
-        console.log(param);
-        i++;
-    }
+if (parseInt(param)) {
+  while (i < x) {
+    console.log(param);
+    i++;
+  }
 }
