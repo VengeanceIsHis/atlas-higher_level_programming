ca4786a986ac9044eaabd9df4a2845c10aeb0ceb
VengeanceIsHis <elijahjpoole@gmail.com>
2024-03-24 17:53:37 -0500

    thank you

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,11 +2,9 @@
 const args = process.argv;
 const param = args[2];
 let i = 0;
-if (parseInt(param))
-{
-    while (i < param)
-    {
-        console.log("C is fun");
-        i++;
-    }
+if (parseInt(param)) {
+  while (i < param) {
+    console.log('C is fun');
+    i++;
+  }
 }
