cd127397422d87d9cc0ee6400fb5027e0fe39c0e
VengeanceIsHis <elijahjpoole@gmail.com>
2024-03-24 19:54:11 -0500

    fix

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -3,15 +3,12 @@ const args = process.argv;
 let i = 2;
 let stored = 0;
 while (i < args.length) {
-    if (i === 2)
-    {
-        if (parseInt(args[i]) > parseInt(args[i + 1]))
-        {
-            stored = args[i];
-            i++;
-        }
+  if (i === 2) {
+    if (parseInt(args[i]) > parseInt(args[i + 1])) {
+      stored = args[i];
+      i++;
     }
-    if (args[i] > stored)
-        stored = args[i];
+  }
+  if (args[i] > stored) { stored = args[i]; }
 }
 console.log(stored);
