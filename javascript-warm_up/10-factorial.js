cc0a34d12e0f76cf6ab48192ddc9be0f113c903c
VengeanceIsHis <elijahjpoole@gmail.com>
2024-03-24 19:33:56 -0500

    fixg

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,15 +1,9 @@
 #!/usr/bin/nodejs
 const args = process.argv;
 const param1 = parseInt(args[2]);
-if (!param1)
-    console.log("1");
-else
-{
-    function factorial(n) {
-        if (n === 0 || n === 1)
-            return (1);
-        else
-        return n * factorial(n - 1);
-    }
-console.log(factorial(param1));
+if (!param1) { console.log('1'); } else {
+  function factorial (n) {
+    if (n === 0 || n === 1) { return (1); } else { return n * factorial(n - 1); }
+  }
+  console.log(factorial(param1));
 }
