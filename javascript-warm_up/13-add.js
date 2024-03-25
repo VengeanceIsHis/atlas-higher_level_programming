a45a6eafebdab2711b3223bf389316783ea58c83
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-24 20:36:49 -0500

    test

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 function add(a, b) {
-let sum = 0;
-  sum += a;
-  sum += b;
-  return sum;
+    let sum = 0;
+    sum += a;
+    sum += b;
+    return sum;
 }
 module.exports = add;
