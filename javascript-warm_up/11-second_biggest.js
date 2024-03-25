23c4b1898d1e18010c1ca720057e5a588b228da7
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-24 19:53:51 -0500

    Adding new file to task 11

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/nodejs
+const args = process.argv;
+let i = 2;
+let stored = 0;
+while (i < args.length) {
+    if (i === 2)
+    {
+        if (parseInt(args[i]) > parseInt(args[i + 1]))
+        {
+            stored = args[i];
+            i++;
+        }
+    }
+    if (args[i] > stored)
+        stored = args[i];
+}
+console.log(stored);
