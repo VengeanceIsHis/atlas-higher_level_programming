7fcf4f9d8be8e5ad064f663ccd297d5d6f661ba7
VengeanceIsHis <elijahjpoole@gmail.com>
2024-03-26 12:40:57 -0500

    fix

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,44 +1,41 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor(w, h) {
-    if (w <= 0 || h <= 0 || !(h) || !(w))
-    {
-        return "Rectangle {}";
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !(h) || !(w)) {
+      return 'Rectangle {}';
+    } else {
+      this.width = w;
+      this.height = h;
     }
-    else
-    {
-    this.width = w;
-    this.height = h;
-    }
-    }
+  }
 
-    print() {
-        let i = 0;
-        let j = 0;
-        let string = "";
+  print () {
+    let i = 0;
+    let j = 0;
+    let string = '';
 
-        if (parseInt(this.width && parseInt(this.height))) {
-            while (j < parseInt(this.width)) {
-              string += 'X';
-              j++;
-            }
+    if (parseInt(this.width && parseInt(this.height))) {
+      while (j < parseInt(this.width)) {
+        string += 'X';
+        j++;
+      }
 
-            while (i < parseInt(this.height)) {
-              console.log(string);
-              i++;
-            }
-          }
+      while (i < parseInt(this.height)) {
+        console.log(string);
+        i++;
+      }
     }
+  }
 
-    rotate() {
-        let temp = this.width;
-        this.width = this.height;
-        this.height = temp;
-    }
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
 
-    double() {
-        this.width *= 2;
-        this.height *= 2;
-    }
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 }
 module.exports = Rectangle;
