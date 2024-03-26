438cb1545fde9cf3a6b723b9bffd0c7b33a0a921
VengeanceIsHis <elijahjpoole@gmail.com>
2024-03-26 12:18:45 -0500

    fix

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,15 +1,12 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor(w, h) {
-    if (w <= 0 || h <= 0 || !(h) || !(w))
-    {
-        return "Rectangle {}";
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !(h) || !(w)) {
+      return 'Rectangle {}';
+    } else {
+      this.width = w;
+      this.height = h;
     }
-    else
-    {
-    this.width = w;
-    this.height = h;
-    }
-    }
+  }
 }
 module.exports = Rectangle;
