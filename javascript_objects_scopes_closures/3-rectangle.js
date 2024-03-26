53fb3d5529ac4ef4632979270b3d465cc2785f5b
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-26 12:31:26 -0500

    testing print statement

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+class Rectangle {
+    constructor(w, h) {
+    if (w <= 0 || h <= 0 || !(h) || !(w))
+    {
+        return "Rectangle {}";
+    }
+    else
+    {
+    this.width = w;
+    this.height = h;
+    }
+    }
+
+    print() {
+        let i = 0;
+        let j = 0;
+        let string = "";
+
+        if (parseInt(this.width && parseInt(this.height))) {
+            while (j < parseInt(this.width)) {
+              string += 'X';
+              j++;
+            }
+
+            while (i < parseInt(this.height)) {
+              console.log(string);
+              i++;
+            }
+          }
+    }
+}
+module.exports = Rectangle;
