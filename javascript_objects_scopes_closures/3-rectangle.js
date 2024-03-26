a17791e6d731cea06c2797610b2698cf957e4a73
VengeanceIsHis <elijahjpoole@gmail.com>
2024-03-26 12:35:04 -0500

    fix

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,33 +1,30 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor(w, h) {
-    if (w <= 0 || h <= 0 || !(h) || !(w))
-    {
-        return "Rectangle {}";
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !(h) || !(w)) {
+      return 'Rectangle {}';
+    } else {
+      this.width = w;
+      this.height = h;
     }
-    else
-    {
-    this.width = w;
-    this.height = h;
-    }
-    }
+  }
 
-    print() {
-        let i = 0;
-        let j = 0;
-        let string = "";
+  print () {
+    let i = 0;
+    let j = 0;
+    let string = '';
 
-        if (parseInt(this.width && parseInt(this.height))) {
-            while (j < parseInt(this.width)) {
-              string += 'X';
-              j++;
-            }
+    if (parseInt(this.width && parseInt(this.height))) {
+      while (j < parseInt(this.width)) {
+        string += 'X';
+        j++;
+      }
 
-            while (i < parseInt(this.height)) {
-              console.log(string);
-              i++;
-            }
-          }
+      while (i < parseInt(this.height)) {
+        console.log(string);
+        i++;
+      }
     }
+  }
 }
 module.exports = Rectangle;
