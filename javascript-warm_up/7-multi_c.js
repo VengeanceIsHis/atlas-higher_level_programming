c0734c08592e8da835dd411a386d7f90fbba1bbc
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-29 16:50:03 -0500

    testing filereader function

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,15 +2,13 @@
 const args = process.argv;
 const param = args[2];
 let i = 0;
-<<<<<<< HEAD
-<<<<<<< HEAD
 if (parseInt(param)) {
   while (i < x) {
     console.log(param);
     i++;
   }
 }
-=======
+
 if (parseInt(param))
 {
     while (i < param)
@@ -19,12 +17,11 @@ if (parseInt(param))
         i++;
     }
 }
->>>>>>> 6eae1827a816fc8c05a345f94f9ab828f5a3afcf
-=======
+
+
 if (parseInt(param)) {
   while (i < param) {
     console.log('C is fun');
     i++;
   }
 }
->>>>>>> 83a449b0e3e7bd45cc07025e531b9b3f1e3e4e66
--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,10 +1,4 @@
 #!/usr/bin/nodejs
-let args = process.argv.splice(2,process.argv.length-1);
-
-args.forEach(function (file){
-        if(args.length >1)
-                console.log(‘========> %s <=========’,file);
-        require(‘fs’).readFile(file, function(err,buf){
-                process.stdout.write(buf);
-        });
-});
+let reader = new FileReader();
+const argument = process.argv;
+reader.readAsDataURL(argument);
