0cf59ed0db34b6214a67f53ddabd158dedd2d2e5
VengeanceIsHis <elijahjpoole@gmail.com>
2024-03-30 17:44:56 -0500

    semistndard

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,13 +1,11 @@
 #!/usr/bin/nodejs
 const fs = require('fs');
 
-
 const argument = process.argv[2];
 fs.readFile(argument, 'utf-8', (err, data) => {
-    if (err) {
-        console.error(err);
-    }
-    else {
-        console.log(data)
-    }
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
 });
