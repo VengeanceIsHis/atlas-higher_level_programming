f22fc68cb90d20b5b9ed523864913b2f62784905
VengeanceIsHis <elijahjpoole@gmail.com>
2024-03-30 18:27:33 -0500

    Fix

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+t request = require('request')
+
+const dataurl = process.argv[2];
+
+request.get(dataurl, (error, response) => {
+	    if (error) {
+		            console.error('Error:', error);
+		        }
+	    else {
+		            console.log('code:', response.statusCode);
+		        }
+});
