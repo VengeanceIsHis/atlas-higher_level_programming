d31f18ee4bf6c767219404365d01aa7add33d1de
VengeanceIsHis <elijahjpoole@gmail.com>
2024-03-30 17:58:42 -0500

    fix

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,10 +1,9 @@
 #!/usr/bin/nodejs
 const fs = require('fs');
 
-
 const filepath = process.argv[2];
 string = process.argv[3];
 
 fs.appendFile(filepath, string, (err) => {
-    if (err) throw err;
+  if (err) throw err;
 });
