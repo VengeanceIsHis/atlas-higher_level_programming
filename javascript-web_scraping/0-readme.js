d1b082037aefcb536354d38df1ed2f70ad1c95ee
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-30 17:43:07 -0500

    fixed

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 
 const argument = process.argv[2];
-fs.readFile(filename, 'utf-8', (err, data) => {
+fs.readFile(argument, 'utf-8', (err, data) => {
     if (err) {
         console.error(err);
     }
