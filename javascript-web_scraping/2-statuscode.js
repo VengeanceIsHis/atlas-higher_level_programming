6264a0ee9d39aa925528350c98a03ba3a7f2c49a
VengeanceIsHis <elijahjpoole@gmail.com>
2024-03-30 18:25:47 -0500

    fix

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,14 +1,13 @@
 #!/usr/bin/nodejs
 const fs = require('fs');
-const request = require('request')
+const request = require('request');
 
 const dataurl = process.argv[2];
 
 request.get(dataurl, (error, response) => {
-    if (error) {
-        console.error('Error:', error);
-    }
-    else {
-        console.log('code:', response.statusCode);
-    }
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    console.log('code:', response.statusCode);
+  }
 });
