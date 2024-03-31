e5cc971ac2301fe4ab68fd46f71fdb1a7b56f8e6
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-31 01:20:56 -0500

    test

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,34 +1,49 @@
 #!/usr/bin/nodejs
 const request = require('request');
 
-
 let count = 0;
 const argument = process.argv[2];
 let i = 0;
 let movieData;
-request.get(argument, (error, response, body) => {
+
+
+const getMovieData = new Promise((resolve, reject) => {
+  request.get(argument, (error, response, body) => {
     if (error) {
-      console.error('Error:', error);
-    }
-    else {
-        movieData = JSON.parse(body);
-    }
-});
-while (i < 7) {
-    individual = argument + '/' + i;
-    console.log(individual)
-    request.get(individual, (error, response, body) => {
-    if (error) {
-      console.error('Error:', error);
-    }
-    else {
-        const indi_movie = JSON.parse(body);
-        if (indi_movie.characters && Array.isArray(indi_movie.characters) && indi_movie.characters.includes("https://swapi-api.hbtn.io/api/people/18/"))
-        {
-            count + 1;
-        }
+      reject(error);
+    } else {
+      resolve(JSON.parse(body));
     }
   });
+});
+
+
+let individualRequests = [];
+
+
+while (i < 7) {
+  const individual = argument + '/' + i;
+  individualRequests.push(new Promise((resolve, reject) => {
+    request.get(individual, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        const indi_movie = JSON.parse(body);
+        if (indi_movie.characters && Array.isArray(indi_movie.characters) && indi_movie.characters.includes("https://swapi-api.hbtn.io/api/people/18/")) {
+          count++;
+        }
+        resolve();
+      }
+    });
+  }));
   i++;
 }
-console.log(count);
+
+
+Promise.all([getMovieData, ...individualRequests])
+  .then(() => {
+    console.log(count);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
