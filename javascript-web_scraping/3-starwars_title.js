891530fc57338893bdcf8fb05704e5eb1dec53fa
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-30 19:35:11 -0500

    there we go

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -2,12 +2,21 @@
 const request = require('request');
 
 argument = process.argv[2];
-movie = "https://swapi-api.hbtn.io/api/films/" + argument;
-request.get(movie, (error, response) => {
+movie = "https://swapi-api.hbtn.io/api/films/";
+
+request.get(movie, (error, response, body) => {
     if (error) {
         console.error('Error:', error);
         }
     else {
-    console.log(response.title);
+        try {
+            const movieData = JSON.parse(body);
+
+            if (movieData.title) {
+                console.log('Movie title:', movieData.title);
+            }
+        } catch (parseError) {
+            console.error('Error parsing JSON:', parseError);
+        }
     }
 });
