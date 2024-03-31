0b489f8eb809497ba6b3c11f2525655ef09269a6
VengeanceIsHis <elijahjpoole@gmail.com>
2024-03-30 19:37:51 -0500

    fix

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -2,21 +2,20 @@
 const request = require('request');
 
 const argument = process.argv[2];
-const movie = "https://swapi-api.hbtn.io/api/films/" + argument;
+const movie = 'https://swapi-api.hbtn.io/api/films/' + argument;
 
 request.get(movie, (error, response, body) => {
-    if (error) {
-        console.error('Error:', error);
-        }
-    else {
-        try {
-            const movieData = JSON.parse(body);
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    try {
+      const movieData = JSON.parse(body);
 
-            if (movieData.title) {
-                console.log(movieData.title);
-            }
-        } catch (parseError) {
-            console.error('Error parsing JSON:', parseError);
-        }
+      if (movieData.title) {
+        console.log(movieData.title);
+      }
+    } catch (parseError) {
+      console.error('Error parsing JSON:', parseError);
     }
+  }
 });
