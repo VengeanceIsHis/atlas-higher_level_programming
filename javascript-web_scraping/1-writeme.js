8f83454338fa74dfa9cc60375791139865d73ba6
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-07-16 13:25:32 -0500

    Delete javascript-web_scraping directory

--- javascript-web_scraping/1-writeme.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/nodejs
-const fs = require('fs');
-
-const filepath = process.argv[2];
-const string = process.argv[3];
-
-fs.appendFile(filepath, string, (err) => {
-  if (err) throw err;
-});
