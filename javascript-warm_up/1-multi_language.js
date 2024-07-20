2b4ba9eb6f76b92675a83b9eef6dec11479fef68
root <root@TokenofElijah>
2024-07-20 17:31:11 -0500

    semi and chmod

--- javascript-warm_up/1-multi_language.js
+++ javascript-warm_up/1-multi_language.js
@@ -1,4 +1,4 @@
 #!/usr/bin/nodejs
-console.log("C is fun")
-console.log("Python is cool")
-console.log("JavaScript is amazing")
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
