36e87cd15799923c5d5d54c2f055513518a644e4
root <root@TokenofElijah>
2024-07-20 17:46:36 -0500

    fix

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -2,6 +2,5 @@
 const args = process.argv.slice(2);
 
 if (args.length === 0) {
-    console.log("No argument")
-}
-else {console.log(args[2])}
+  console.log('No argument');
+} else { console.log(args[2]); }
