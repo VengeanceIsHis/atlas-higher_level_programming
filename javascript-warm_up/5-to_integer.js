fd6311cee427e5b87f027926761023f8113555a6
root <root@TokenofElijah>
2024-07-20 18:30:02 -0500

    Fix

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -2,8 +2,9 @@
 if (process.argv[2]) {
 
 }
-let intvalue = parseInt(process.argv[2], 10);
+const intvalue = parseInt(process.argv[2], 10);
 if (isNaN(intvalue)) {
-    console.log("Not a number")}
-else {
-console.log("My number:", intvalue); }
+  console.log('Not a number');
+} else {
+  console.log('My number:', intvalue);
+}
