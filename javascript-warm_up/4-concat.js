ea4067586577811db348ff59593f17114ecec747
root <root@TokenofElijah>
2024-07-20 18:06:46 -0500

    fix

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,4 +1,4 @@
 #!/usr/bin/nodejs
-let one = process.argv[2] || 'undefined';
-let two = process.argv[3] || 'undefined';
+const one = process.argv[2] || 'undefined';
+const two = process.argv[3] || 'undefined';
 console.log(one, 'is', two);
