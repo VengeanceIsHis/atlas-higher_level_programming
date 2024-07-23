ebee386290ca237bef8ee3c4affb82159e317312
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-07-23 15:34:13 -0500

    Adding task 7

---
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,9 @@
+exports.nb0ccurrences = function(list, searchElement) {
+    let count = 0;
+    for (let i = 0; i < list.length; i++)
+        if (list[i] = searchElement)
+        {
+            count++;
+        }
+    console.log(count)
+}
