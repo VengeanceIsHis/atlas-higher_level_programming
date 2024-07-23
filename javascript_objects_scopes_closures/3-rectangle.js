1de6deb5f62985f938370439cc00e4dbf2e271cd
root <root@TokenofElijah>
2024-07-23 14:45:48 -0500

    semistandard

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,21 +1,20 @@
 #!/usr/bin/nodejs
 class Rectangle {
-    constructor(w, h) {
-        if (w > 0 && h > 0) {
-            this.width = w;
-            this.height = h;
-        }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-    print() {
-        let output = "";
-            for (let i = 0; i < this.width; i++)
-            {
-                output += "X";
-            }
-            for (let j = 0; j < this.height; j++)
-            {
-                console.log(output);
-            }
+  }
+
+  print () {
+    let output = '';
+    for (let i = 0; i < this.width; i++) {
+      output += 'X';
     }
+    for (let j = 0; j < this.height; j++) {
+      console.log(output);
+    }
+  }
 }
 module.exports = Rectangle;
