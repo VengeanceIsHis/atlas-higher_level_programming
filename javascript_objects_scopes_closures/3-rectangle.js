ce9b0d616c04e761d39d1b4ce685a6cacc5caa15
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-07-23 14:38:18 -0500

    adding let to variable

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -9,7 +9,7 @@ class Rectangle {
     print() {
         for (let i = 0; i < this.height; i++)
         {
-            output = ""
+           let output = ""
             for (let j = 0; j < this.width; j++)
             {
                 output += "X"
