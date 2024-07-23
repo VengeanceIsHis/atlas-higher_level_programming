670ec19fad6331033f5c23053075ed44d9f920e9
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-07-23 00:59:32 -0500

    there

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,11 +1,8 @@
 #!/usr/bin/nodejs
-const one = parseInt(process.argv[2], 10);
-const two = parseInt(process.argv[3], 10);
 function add (a, b) {
   const result = a + b;
   return result;
 }
-console.log(add(one, two));
 module.exports = {
     add
 };
