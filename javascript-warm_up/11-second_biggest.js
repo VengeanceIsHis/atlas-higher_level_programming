be7f06134595949071265a8a0d0df6c6192ee9f3
root <root@TokenofElijah>
2024-07-23 00:46:08 -0500

    semi

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -2,9 +2,9 @@
 const args = process.argv.slice(2);
 
 if (args.length === 0 || args.length === 1) {
-    console.log('0');
+  console.log('0');
 } else {
-    const numbers = args.map(arg => parseInt(arg, 10));
-    const sortedNumbers = numbers.sort((a, b) => b - a);
-    console.log(sortedNumbers[1]);
+  const numbers = args.map(arg => parseInt(arg, 10));
+  const sortedNumbers = numbers.sort((a, b) => b - a);
+  console.log(sortedNumbers[1]);
 }
