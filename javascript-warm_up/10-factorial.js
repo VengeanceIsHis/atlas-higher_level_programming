599cf91edb5cfca68010fc4a97a780d2455fafc6
root <root@TokenofElijah>
2024-07-23 00:35:27 -0500

    fix

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,11 +1,11 @@
 #!/usr/bin/nodejs
 const intvalue = parseInt(process.argv[2], 10);
-function factorial(n) {
-    if (isNaN(n) || n === 0) {
-        return 1;
-    } else {
-        return n * factorial(n - 1);
-    }
+function factorial (n) {
+  if (isNaN(n) || n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 }
 const result = factorial(intvalue);
 console.log(result);
