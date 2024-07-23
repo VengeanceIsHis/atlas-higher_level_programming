5c5b077871fbb84c1022b2c3bb744fddd9e2546c
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-07-23 14:59:48 -0500

    adding ;

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,5 +1,5 @@
 #!/usr/bin/nodejs
-const Rectangle = require("./4-rectangle")
+const Rectangle = require('./4-rectangle');
 class Square extends Rectangle {
     constructor(size) {
         super(size, size);
