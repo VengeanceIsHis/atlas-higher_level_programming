110f6b1bce5b0bdf8bda9f25d9a615c9ac114cb5
root <root@TokenofElijah>
2024-07-23 00:07:06 -0500

    fix

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,12 +1,10 @@
 #!/usr/bin/nodejs
 const args = process.argv;
-let intvalue = parseInt(process.argv[2], 10);
+const intvalue = parseInt(process.argv[2], 10);
 if (isNaN(intvalue)) {
-    console.log('Missing number of occurrences');
-}
-else {
-    for (let i = 0; i < intvalue; i++)
-    {
-        console.log('C is fun');
-    }
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < intvalue; i++) {
+    console.log('C is fun');
+  }
 }
