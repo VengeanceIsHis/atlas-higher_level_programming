e0ef44b6ed6b4607808e4b45bd40489fc9b33889
root <root@TokenofElijah>
2024-07-23 00:19:05 -0500

    fix

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,14 +1,12 @@
 #!/usr/bin/nodejs
-let intvalue = parseInt(process.argv[2], 10);
+const intvalue = parseInt(process.argv[2], 10);
 if (isNaN(intvalue)) {
-    console.log('Missing size');
+  console.log('Missing size');
+}
+for (let x = 0; x < intvalue; x++) {
+  let output = '';
+  for (let y = 0; y < intvalue; y++) {
+    output += 'X';
   }
-for (let x = 0; x < intvalue; x++)
-{
-    let output = "";
-    for (let y = 0; y < intvalue; y++)
-    {
-        output += "X"; 
-    }
-    console.log(output);
+  console.log(output);
 }
