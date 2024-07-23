746c897e0f0f900195678186d6ecc0948aa64d3f
root <root@TokenofElijah>
2024-07-23 15:22:46 -0500

    fix semistandard

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,20 +1,18 @@
 #!/usr/bin/nodejs
 const BaseSquare = require('./5-square');
 class Square extends BaseSquare {
-    constructor(size) {
-        super(size, size);
-    }
-    charPrint(c = "X") {
+  constructor (size) {
+    super(size, size);
+  }
 
-        let output = "";
-        for (let j = 0; j < this.height; j++)
-        {
-            output += c;
-        }
-        for (let i = 0; i < this.height; i++)
-        {
-            console.log(output);
-        }
+  charPrint (c = 'X') {
+    let output = '';
+    for (let j = 0; j < this.height; j++) {
+      output += c;
     }
+    for (let i = 0; i < this.height; i++) {
+      console.log(output);
+    }
+  }
 }
 module.exports = Square;
