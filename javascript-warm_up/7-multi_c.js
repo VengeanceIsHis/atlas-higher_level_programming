4d0083a9717bf9a17ff81d018e677234a0037334
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-07-23 00:09:16 -0500

    fixed

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,10 +1,5 @@
 #!/usr/bin/nodejs
-<<<<<<< HEAD
-const args = process.argv;
-const intvalue = parseInt(process.argv[2], 10);
-=======
 let intvalue = parseInt(process.argv[2], 10);
->>>>>>> refs/remotes/origin/master
 if (isNaN(intvalue)) {
   console.log('Missing number of occurrences');
 } else {
