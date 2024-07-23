6e3308edefeb8fbf64664a62a1f362c7b1a97c0c
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-07-23 00:03:27 -0500

    fixed method call

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,5 +1,5 @@
 #!/usr/bin/nodejs
-let intvalue = parseint(process.argv[2], 10);
+let intvalue = parseInt(process.argv[2], 10);
 if (isNaN(intvalue)) {
     console.log('Missing number of occurrences');
 }
