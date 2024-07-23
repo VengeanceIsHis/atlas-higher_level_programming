63b4aec18b7f5d3779134d495ee3889687daf6ec
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-07-23 15:39:09 -0500

    changed name

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,5 @@
 #!/usr/bin/nodejs
-exports.nb0ccurrences = function(list, searchElement) {
+exports.nbOccurrences = function(list, searchElement) {
     let count = 0;
     for (let i = 0; i < list.length; i++)
         if (list[i] === searchElement)
