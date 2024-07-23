914c0397381cb6f451aa3bbd99dbf16f1892a3be
root <root@TokenofElijah>
2024-07-23 14:14:48 -0500

    semistandard task 2

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/nodejs
 class Rectangle {
-    constructor(w, h) {
-        this.width = w;
-        this.height = h;
-    }
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 }
 module.exports = Rectangle;
