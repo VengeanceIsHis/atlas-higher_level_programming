ab8f5e0d4aead7e1cb6b0e7b5f8716d9b4c8f193
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-07-23 15:21:34 -0500

    test

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -7,11 +7,11 @@ class Square extends BaseSquare {
     charPrint(c = "X") {
 
         let output = "";
-        for (let j = 0; j < this.size; j++)
+        for (let j = 0; j < this.height; j++)
         {
             output += c;
         }
-        for (let i = 0; i < this.size; i++)
+        for (let i = 0; i < this.height; i++)
         {
             console.log(output);
         }
