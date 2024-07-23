a9bcc148b71103733dd22c46a22c33ccf02bac07
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-07-23 00:02:38 -0500

    test for task 7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,11 +1,10 @@
 #!/usr/bin/nodejs
-const args = process.argv;
 let intvalue = parseint(process.argv[2], 10);
 if (isNaN(intvalue)) {
     console.log('Missing number of occurrences');
 }
 else {
-    for (let i = 2; i < args.length; i++)
+    for (let i = 2; i < process.argv.length; i++)
     {
         console.log('C is fun');
     }
