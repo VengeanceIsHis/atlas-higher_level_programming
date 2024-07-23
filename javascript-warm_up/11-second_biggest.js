e43ae81e4df36a795eb5e7c4b82a70dc66290492
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-07-23 00:45:27 -0500

    second biggest task 11

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/nodejs
+const args = process.argv.slice(2);
+
+if (args.length === 0 || args.length === 1) {
+    console.log('0');
+} else {
+    const numbers = args.map(arg => parseInt(arg, 10));
+    const sortedNumbers = numbers.sort((a, b) => b - a);
+    console.log(sortedNumbers[1]);
+}
