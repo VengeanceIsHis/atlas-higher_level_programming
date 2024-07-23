2c08321abeb910737abf49f43267207156b54c4d
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-07-23 00:18:37 -0500

    changing loop

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,10 +1,14 @@
 #!/usr/bin/nodejs
-const intvalue = parseInt(process.argv[2], 10);
+let intvalue = parseInt(process.argv[2], 10);
 if (isNaN(intvalue)) {
-  console.log('Missing size');
-}
-for (let x = 0; x < intvalue; x++) {
-  for (let y = 0; y < intvalue; y++) {
-    console.log('X');
+    console.log('Missing size');
   }
+for (let x = 0; x < intvalue; x++)
+{
+    let output = "";
+    for (let y = 0; y < intvalue; y++)
+    {
+        output += "X"; 
+    }
+    console.log(output);
 }
