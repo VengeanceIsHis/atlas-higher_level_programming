7142d839744a934887ae326d281e36924f37279e
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-07-23 14:21:50 -0500

    Adding task 2

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/nodejs
+class Rectangle {
+    constructor(w, h) {
+        if (w >= 0 || isNaN(w)) {
+            this.width = "";
+        }
+        else {
+            this.width = w;
+        }
+        if (h >= 0 || isNaN(h)) {
+            this.height = "";
+        }
+        else {
+            this.height = h;
+        }
+}
+}
+module.exports = Rectangle;
