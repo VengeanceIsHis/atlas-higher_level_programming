9e8d66e5aca43a19b91812f37e4772f8a7555063
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-07-23 14:28:41 -0500

    Changing if statement

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,7 @@
 #!/usr/bin/nodejs
 class Rectangle {
     constructor(w, h) {
-        if (w <= 0 || isNaN(w) || h <= 0) {
-            pass;
-        }
-        else {
+        if (w > 0 || h > 0) {
             this.width = w;
             this.height = h;
         }
