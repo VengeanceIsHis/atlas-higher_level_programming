02799aec6795dece6fedd4ea0b04bcff1eb5ed38
root <root@TokenofElijah>
2024-07-23 00:24:56 -0500

    fix

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,9 +1,8 @@
 #!/usr/bin/nodejs
-let one = parseInt(process.argv[2], 10);
-let two = parseInt(process.argv[3], 10);
-function add(a, b)
-{
-    result = a + b;
-    return result
+const one = parseInt(process.argv[2], 10);
+const two = parseInt(process.argv[3], 10);
+function add (a, b) {
+  result = a + b;
+  return result;
 }
-console.log(add(one, two))
+console.log(add(one, two));
