621f5b8a558905b16af6b27dc5eeec64340c5242
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-07-27 17:03:36 -0500

    adding task 4 to git

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/nodejs
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, (err, response, body) => {
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+    const data = JSON.parse(body);
+    let count = 0;
+
+    data.result.forEach(film => {
+        const hasWedgeAntilles = film.characters.some(characterUrl => {
+            return characterUrl.endswith(`/${wedgeAntillesId}/`);
+        });
+        if (hasWedgeAntilles) {
+            count++;
+        }
+    });
+    console.log(count);
+});
