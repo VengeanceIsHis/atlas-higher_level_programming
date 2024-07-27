019ec9318d37c0c7372b0468798c236c2f4ce180
root <root@TokenofElijah>
2024-07-27 15:58:04 -0500

    semistandard

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/nodejs
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(data);
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
