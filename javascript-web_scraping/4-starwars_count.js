f0088084a539b9eb013b8fca9d254725367457ff
root <root@TokenofElijah>
2024-07-27 17:09:07 -0500

    ejp

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -3,22 +3,21 @@ const request = require('request');
 const url = process.argv[2];
 const wedgeAntillesId = '18';
 
-
 request.get(url, (err, response, body) => {
-    if (err) {
-      console.error(err.message);
-      return;
-    }
-    const data = JSON.parse(body);
-    let count = 0;
+  if (err) {
+    console.error(err.message);
+    return;
+  }
+  const data = JSON.parse(body);
+  let count = 0;
 
-    data.results.forEach(film => {
-        const hasWedgeAntilles = film.characters.some(characterUrl => {
-            return characterUrl.endsWith(`/${wedgeAntillesId}/`);
-        });
-        if (hasWedgeAntilles) {
-            count++;
-        }
+  data.results.forEach(film => {
+    const hasWedgeAntilles = film.characters.some(characterUrl => {
+      return characterUrl.endsWith(`/${wedgeAntillesId}/`);
     });
-    console.log(count);
+    if (hasWedgeAntilles) {
+      count++;
+    }
+  });
+  console.log(count);
 });
