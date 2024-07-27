bec4987e42e116db6ecc80be9fbcd2c192120cdb
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-07-27 16:16:05 -0500

    adding error

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -4,4 +4,6 @@ const url = process.argv[2];
 
 https.get(url, (res) => {
     console.log('Status Code:', res.statusCode);
-    });
+}).on('error', (err) => {
+    console.error('Error:', err.message);
+});
