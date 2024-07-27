fe1f871a54ea7e6c44cd410d567093b243d66c19
root <root@TokenofElijah>
2024-07-27 16:37:17 -0500

    EJP

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -4,10 +4,10 @@ let url = 'https://swapi-api.hbtn.io/api/films/';
 url += process.argv[2];
 
 request.get(url, (err, response, body) => {
-    if (err) {
-         console.error(err.message);
-         return;
-    }
-    const data = JSON.parse(body);
-    console.log(data.title)
+  if (err) {
+    console.error(err.message);
+    return;
+  }
+  const data = JSON.parse(body);
+  console.log(data.title);
 });
