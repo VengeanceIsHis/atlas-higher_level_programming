d198a3dac2453b9e52dedc94c0bc844bd2f11ce4
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-07-29 18:06:53 -0500

    test

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -6,10 +6,16 @@ const file_name = process.argv[3];
 
 request.get(url, (err, response, body) => {
     if (err) {
-      console.error(err.message);
-      return;
+        console.error('Error fetching the URL:', err.message);
+        return;
     }
 
-    fs.writeFile(file_name, body)
-  });
-  
+    // Write the body to the file
+    fs.writeFile(file_name, body, (err) => {
+        if (err) {
+            console.error('Error writing to file:', err.message);
+        } else {
+            console.log(`Content saved to ${file_name}`);
+        }
+    });
+});
