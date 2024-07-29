1986b1d43fef9c32d2f3a81bc17ffa97286e7328
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-07-29 17:54:29 -0500

    If only

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/nodejs
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const file_name = process.argv[3];
+
+request.get(url, (err, response, body) => {
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+    const data = JSON.parse(body);
+
+    fs.writeFile(file_name, data)
+  });
+  
