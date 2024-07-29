4aac3df9b0cc64737e3ee8ed0344040eddb5b5c9
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-07-29 18:12:25 -0500

    changing variable name

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -2,7 +2,7 @@
 const request = require('request');
 const fs = require('fs');
 const url = process.argv[2];
-const file_name = process.argv[3];
+const filename = process.argv[3];
 
 request.get(url, (err, response, body) => {
     if (err) {
@@ -11,7 +11,7 @@ request.get(url, (err, response, body) => {
     }
 
     // Write the body to the file
-    fs.writeFile(file_name, body, (err) => {
+    fs.writeFile(filename, body, (err) => {
         if (err) {
             console.error('Error writing to file:', err.message);
         }
