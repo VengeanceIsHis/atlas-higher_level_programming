21eb38625db917567837f09ca8fc5eb7b73e7b9e
root <root@TokenofElijah>
2024-07-29 18:13:40 -0500

    fix

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -5,15 +5,15 @@ const url = process.argv[2];
 const file_name = process.argv[3];
 
 request.get(url, (err, response, body) => {
-    if (err) {
-        console.error('Error fetching the URL:', err.message);
-        return;
-    }
+  if (err) {
+    console.error('Error fetching the URL:', err.message);
+    return;
+  }
 
-    // Write the body to the file
-    fs.writeFile(file_name, body, (err) => {
-        if (err) {
-            console.error('Error writing to file:', err.message);
-        }
-    });
+  // Write the body to the file
+  fs.writeFile(file_name, body, (err) => {
+    if (err) {
+      console.error('Error writing to file:', err.message);
+    }
+  });
 });
