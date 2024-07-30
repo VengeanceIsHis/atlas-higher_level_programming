14c8c2955d28d7b8fdf23b0932b8f518b5782d42
root <root@TokenofElijah>
2024-07-30 16:40:46 -0500

    fix

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -3,20 +3,20 @@ const request = require('request');
 const url = process.argv[2];
 
 request.get(url, (err, response, body) => {
-    if (err) {
-      console.error(err.message);
-      return;
+  if (err) {
+    console.error(err.message);
+    return;
+  }
+  const result = {};
+  const data = JSON.parse(body);
+  data.forEach(task => {
+    const { userId, completed } = task;
+    if (completed) {
+      if (!result[userId]) {
+        result[userId] = 0;
+      }
+      result[userId]++;
     }
-    let result = {};
-    const data = JSON.parse(body);
-    data.forEach(task => {
-        const { userId, completed } = task;
-        if (completed) {
-            if (!result[userId]) {
-                result[userId] = 0;
-            }
-            result[userId]++;
-        }
-    });
-    console.log(result);
+  });
+  console.log(result);
 });
