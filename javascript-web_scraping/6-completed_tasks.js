5728bbf7dbbf40b9269edea21c8c009b063092ee
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-07-30 16:40:07 -0500

    tset

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -3,22 +3,20 @@ const request = require('request');
 const url = process.argv[2];
 
 request.get(url, (err, response, body) => {
-  if (err) {
-    console.error(err.message);
-    return;
-  }
-  const result = {};
-  const data = JSON.parse(body);
-  const i = 0;
-  data.forEach(task => {
-    const { userId, completed } = task;
-
-    if (!result[userId]) {
-      result[userId] = 0;
+    if (err) {
+      console.error(err.message);
+      return;
     }
-    if (completed) {
-      result[userId] = (result[userId] || 0) + 1;
-    }
-  });
-  console.log(result);
+    let result = {};
+    const data = JSON.parse(body);
+    data.forEach(task => {
+        const { userId, completed } = task;
+        if (completed) {
+            if (!result[userId]) {
+                result[userId] = 0;
+            }
+            result[userId]++;
+        }
+    });
+    console.log(result);
 });
