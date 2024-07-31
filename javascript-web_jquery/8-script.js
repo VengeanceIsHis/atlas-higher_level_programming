5d3b795f9b6528a1eb507bc4b1ed630475c03a2c
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-07-31 09:49:06 -0500

    test

--- javascript-web_jquery/8-script.js
+++ javascript-web_jquery/8-script.js
@@ -1,6 +1,11 @@
-
 $(document).ready(function() {
     $.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function(data) {
-    $('#list_movies').append(data.title)
+    const films = data.films;
+    films.forEach(function(filmUrl) {
+        $.get(filmUrl, function(filmData) {
+            const listItem = $('<li>').text(filmData.title);
+            $('#list_movies').append(listItem);
+        })
+    })
     })
 })
