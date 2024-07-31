d653f4d44a8b2f4831abdc98ab5861e0e2132963
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-07-30 20:09:00 -0500

    test

--- javascript-web_jquery/2-script.js
+++ javascript-web_jquery/2-script.js
@@ -1,11 +1,5 @@
 $(document).ready(function() {
-    $('#red_header').css({
-        'cursor': 'pointer',
-        'border': '1px solid black',
-        'padding': '10px',
-        'background-color': '#e0e0e0'
-    });
-    $('red_header').on('click', function() {
+    $('#red_header').on('click', function() {
         $('header').css('color', '#FF0000');
     });
 });
